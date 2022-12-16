f26daf477da2b758fb1e30f19b51881ae4e14c26
unknown <ytewachew168@gmail.com>
2022-12-16 09:09:39 -0800

    First commit

---
+++ src/main/java/com/emonics/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.emonics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+	@GetMapping("/")
+	public String saveHello(Model model) {
+		model.addAttribute("message", "Hello Jenkins");
+		return "greeting";
+	}
+}
